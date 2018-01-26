f645ddf13f9d16f41f2f91b8649852b476d606f7
Jordan Ford <jsford94@gmail.com>
2018-01-26 02:02:52 -0500

    Moved teensy and ros_teensy into a src/teensy/ directory.

--- catkin_ws/src/teensy/teensy/src/main.cpp
+++ catkin_ws/src/teensy/teensy/src/main.cpp
@@ -3,10 +3,11 @@
 #define LED_PIN 13
 
 int main(void) {
+
     pinMode(LED_PIN, OUTPUT);
     while(1) {
         digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-        delay(1000);
+        delay(100);
     }
 
     return 0;
